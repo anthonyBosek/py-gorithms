76497515de614a73ad12604422183e0ea46f651d
Anthony Bosek <anthony.bosek@gmail.com>
2023-05-16 11:27:19 -0700

    assignment dir setup

---
+++ zzz/CSc-120/Assignments/Assign_One/list_to_string_v1.py
@@ -0,0 +1,1 @@
+# List to string (Version 1)
---
+++ zzz/CSc-120/Reviews/ICA_3.py
@@ -0,0 +1,41 @@
+# Dictionary usage for reference:
+# >>> d = {}
+# >>> d['one'] = 1
+# >>> d['two'] = 2
+# >>> d['five'] = 5
+# >>> 'three' in d
+# False
+
+# ---------------------------------------------------------------
+
+# 1. Write a loop to print the keys of d.
+
+# ---------------------------------------------------------------
+
+# 2. Write a loop to print the values in d two different ways:
+# a) use the values() method
+# b) don’t use the values() method
+
+# ---------------------------------------------------------------
+
+# 3. Write a function key_of_max_value(adict) that finds the maximum of all the values in
+# the dictionary adict and returns the corresponding key. For example, if the dictionary passed
+# in is
+# {"hello" : 34, "sunny" : 51, "the" : 82, "street" : 13}
+# then the function returns the key the.
+# You’ll have to iterate through the dictionary and keep track of the maximum value seen so far,
+# but also keep track of the corresponding key for that value.
+
+# ---------------------------------------------------------------
+
+# 4. Create a function identify_unique_words(slist) that takes a list of strings slist.
+# The function returns a dictionary where the keys are the strings in slist and the
+# corresponding values are 0, if the string occurred only once in slist, and 1 otherwise. For
+# example, if the function is called with the list
+# ['here', 'is', 'the', 'root', 'of', 'the', 'root', 'and', 'the']
+# then the dictionary returned is
+# {'here': 0, 'is': 0, 'the': 1, 'root': 1, 'of': 0, 'and': 0}
+# Notice that the strings that are unique in slist have a value of 0, and the words that are
+# duplicates have a value of 1.
+
+# ---------------------------------------------------------------
