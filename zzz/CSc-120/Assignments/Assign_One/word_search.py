0294ca4ba80007ede655fe980c5e4d2c88fd45b8
Anthony Bosek <anthony.bosek@gmail.com>
2023-05-19 07:48:16 -0700

    begin word search assign

--- zzz/CSc-120/Assignments/Assign_One/word_search.py
+++ zzz/CSc-120/Assignments/Assign_One/word_search.py
@@ -1,1 +1,22 @@
 # Word Serach
+
+# word-list.txt file of words
+# grid-of-letters.txt file of grid letters
+
+# 1. Read in the word-list.txt file and store the words in a list.
+# 2. Read in the grid-of-letters.txt file and store the letters in a list of lists.
+# 3. For each word in the word list, search for it in the grid of letters.
+# 4. If the word is found, append it to a list of found words.
+# 5. Print the list of found words.
+
+
+def occurs_in(substr, word_list):
+    # ignore casing
+    # check horizontal
+    # check vertical
+    # check diagonal
+    return
+
+
+def main():
+    occurs_in()
