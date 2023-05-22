b3ab4c02625e495cd9de53c3a1739809d7e2f608
Anthony Bosek <anthony.bosek@gmail.com>
2023-05-21 20:05:11 -0700

    update cv_match solution

--- zzz/CSc-120/Assignments/Assign_One/cv_match.py
+++ zzz/CSc-120/Assignments/Assign_One/cv_match.py
@@ -18,9 +18,7 @@ def cv_match(sentence, pattern):
 print(
     cv_match("Tim has a pet rat named Nip", "cvc")
 )  # ➞ ['Tim', 'has', 'pet', 'rat', 'Nip']
-
 print(cv_match("Put the loot in your boot", "cvvc"))  # ➞ ['loot', 'your', 'boot']
-
 print(cv_match("This will have no matches", "vvv"))  # ➞ []
-print(cv_match("This will have matches latches", "cvcccvc"))  # ➞ [matches]
-print(cv_match("This will have gave matches", "cvcv"))  # ➞ [have]
+print(cv_match("This will have matches latches", "cvcccvc"))  # ➞ [matches, latches]
+print(cv_match("This will have gave matches", "cvcv"))  # ➞ [have, gave]
